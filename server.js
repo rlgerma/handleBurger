d9253d503d96205c83fb0f535213ce78a2df1988
Richard Germaine <rlgerma92@outlook.com>
2019-12-15 15:54:18 -0800

    test

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const express = require("express");
+var express = require("express");
 
-const PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 8080;
 
-const app = express();
+var app = express();
 
 app.use(express.static("public"));
 
@@ -10,7 +10,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Set Handlebars.
-const exphbs = require("express-handlebars");
+var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
